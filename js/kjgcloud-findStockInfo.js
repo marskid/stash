30971068e757c0d7e8db4d5fd5f443f8b9f881d8
Melvin Wang <marskid@users.noreply.github.com>
2023-07-28 09:52:51 +0800

    Create kjgcloud-findStockInfo.js

---
+++ js/kjgcloud-findStockInfo.js
@@ -0,0 +1,31 @@
+const url = $request.url;
+const method = $request.method;
+const notifyTitle = "抢票插件";
+
+if (url.indexOf('findStockInfo') == -1) {
+  $done({});
+}
+
+if (method !== "POST") {
+    $done({});
+}
+
+if (!$response.body) {
+    $done({});
+}
+
+let body = JSON.parse($response.body);
+
+if (!body.data || body.code != "0") {
+    console.log(url);
+    console.log(`body:${$response.body}`);
+    $notification.post(notifyTitle, url, "data字段错误");
+} else {
+  body.data.forEach(i => {
+    i.stockCount = 1000;
+    i.usedCount = 100;
+  });
+  $done({
+    body
+  });
+}
