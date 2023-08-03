fd4173ae38dcbda8825a6d2f95efe8729b2a2757
Melvin Wang <marskid@users.noreply.github.com>
2023-08-03 16:21:44 +0800

    Update kjgcloud.js

--- js/kjgcloud.js
+++ js/kjgcloud.js
@@ -1,8 +1,10 @@
+console.log($environment);
+
 const url = $request.url;
 const method = $request.method;
 const notifyTitle = "抢票插件";
 
-console.log($environment);
+
 
 if (!$response.body) {
     $done({});
